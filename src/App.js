3145282565111997a12a2a2de79746ff418d4e93
NABASA ISAAC <135027171+nabasaisaac@users.noreply.github.com>
2025-05-05 19:09:41 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
- React, { useState } from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
